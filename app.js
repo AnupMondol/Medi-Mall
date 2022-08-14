dead55adaecbfb7a4ea315d55e802bdc9a06930b
AnupMondol <anupmondol007@gmail.com>
2022-08-14 11:57:36 +0600

    final cut

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const id = document.getElementById('nav-bar');
-id.addEventListener('click', function(){
+function showNav(){
     document.getElementById('all').style.display = 'block';
-})
+    document.getElementById('help-logo').style.display = 'none';
+
+}
+
+function hideNav(){
+    document.getElementById('all').style.display = 'none';
+    document.getElementById('help-logo').style.display = 'block';
+}
+    
+       
+
+        
+
 
